d4f351a8b939075fa9f3ec7b17b4adf72ac672f0
roshanlabh <roshanlabh@gmail.com>
2018-05-24 15:55:36 +0530

    fixed mongodb connection for heroku

--- env.js
+++ env.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  // process.env.databaseUri = 'mongodb://patel:patel@ds153752.mlab.com:53752/angular-2-app'; // Databse URI and database name
-  process.env.databaseUri = 'mongodb://localhost:27017/mean-angular-2'; // Databse URI and database name
-  process.env.databaseName = 'production database: mean-angular-2'; // Database name
+  // process.env.databaseUri = 'mongodb://localhost:27017/mean-angular-2'; // Databse URI and database name
+  process.env.databaseUri = 'mongodb://mbloguser:1234@ds016298.mlab.com:16298/rambo-mblog'; // Databse URI and database name
+  process.env.databaseName = 'production database: rambo-mblog'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
   process.env.databaseUri = 'mongodb://localhost:27017/mean-angular-2'; // Databse URI and database name
