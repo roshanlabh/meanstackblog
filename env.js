d864553ff0d34beb261c3135648a9d35d666fb00
Roshan Labh <roshanlabh@gmail.com>
2018-06-11 12:12:31 +0530

    updated env.js
    updated env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 if (fs.existsSync('./public')) {
   process.env.NODE_ENV = 'production';
-  process.env.databaseUri = 'mongodb://mbloguser:1234@ds016298.mlab.com:16298/rambo-mblog'; // Databse URI and database name
+  process.env.databaseUri = ''; // Databse URI and database name
   process.env.databaseName = 'production database: rambo-mblog'; // Database name
 } else {
   process.env.NODE_ENV = 'development';
